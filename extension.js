c9d2ddb7d2a6704a1f19ac2819f54e8b4fd402a1
geoph9 <geoph9@protonmail.com>
2021-01-21 15:27:33 +0200

    add schema (compiled) and read settings from extension

--- extension.js
+++ extension.js
@@ -1,10 +1,38 @@
+const Me = imports.misc.extensionUtils.getCurrentExtension();
+const Gio = imports.gi.Gio;
+
 const {St, Clutter} = imports.gi;
 const Main = imports.ui.main;
 
 let panelButton;
 
+// const GLib = imports.gi.GLib;
+// let now = GLib.DateTime.new_now_local();
+// let nowString = now.format("%Y-%m-%d %H:%M:%S");
+
 // e.g. notify
 //Main.notify('Message Title', 'Message Body');
+
+// const Mainloop = imports.mainloop;
+// let timeout = Mainloop.timeout_add_seconds(2.5, () => {
+//   // this function will be called every 2.5 seconds
+// });
+// // remove mainloop
+// Mainloop.source_remove(timeout);
+
+function getSettings () {
+  let GioSSS = Gio.SettingsSchemaSource;
+  let schemaSource = GioSSS.new_from_directory(
+    Me.dir.get_child("schemas").get_path(),
+    GioSSS.get_default(),
+    false
+  );
+  let schemaObj = schemaSource.lookup('org.gnome.shell.extensions.hass-data', true);
+  if (!schemaObj) {
+    throw new Error('cannot find schemas');
+  }
+  return new Gio.Settings({ settings_schema : schemaObj });
+}
 
 function init () {
     // Create a Button with "Hello World" text
@@ -19,6 +47,11 @@ function init () {
 }
 
 function enable () {
+    let settings = getSettings();
+    // Can also use settings.set_string('...', '...');
+    // settings.get_string('hass-url');
+    // settings.get_string('hass-access-token');
+    
     // Add the button to the panel
     Main.panel._rightBox.insert_child_at_index(panelButton, 0);
 }
